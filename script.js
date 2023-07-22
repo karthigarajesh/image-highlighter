fa22ae253029066da475a91b6f8e99f9ce9f2bc3
karthigarajesh <140259115+karthigarajesh@users.noreply.github.com>
2023-07-22 21:24:11 +0530

    Image college in html ass-3

---
+++ script.js
@@ -0,0 +1,25 @@
+const current = document.querySelector("#current");
+const imgs = document.querySelector(".imgs");
+const img = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+
+console.log(img);
+
+img[0].style.opacity = opacity;
+
+imgs.addEventListener("click", (event)  => {
+    img.forEach((imgElem) => {
+        imgElem.style.opacity = 1;
+    });
+    const clickedImgUrl = event.target.src;
+
+    current.src= clickedImgUrl;
+    current.classList.add("fade-in");
+
+    setTimeout(() => {
+        current.classList.remove("fade-in");
+    },600);
+    event.target.style.opacity = opacity;
+});
+ 
+
